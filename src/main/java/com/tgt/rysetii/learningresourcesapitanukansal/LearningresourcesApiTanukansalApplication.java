2c2d948256717a588d9b560114b7b8f608d9ae8e
tkansal18 <er.tanukansal@gmail.com>
2022-07-10 13:46:28 +0530

    first commit

---
+++ src/main/java/com/tgt/rysetii/learningresourcesapitanukansal/LearningresourcesApiTanukansalApplication.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningresourcesapitanukansal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcesApiTanukansalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningresourcesApiTanukansalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgt/rysetii/learningresourcesapitanukansal/LearningresourcesApiTanukansalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningresourcesapitanukansal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningresourcesApiTanukansalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
