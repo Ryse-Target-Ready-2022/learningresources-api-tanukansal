14773b1e78350fe455c928dbaa24e28777ffc230
tkansal18 <er.tanukansal@gmail.com>
2022-07-12 11:48:32 +0530

    added class and enum with constructor

--- src/main/java/com/tgt/rysetii/learningresourcesapitanukansal/LearningresourcesApiTanukansalApplication.java
+++ src/main/java/com/tgt/rysetii/learningresourcesapitanukansal/LearningresourcesApiTanukansalApplication.java
@@ -8,7 +8,7 @@ public class LearningresourcesApiTanukansalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningresourcesApiTanukansalApplication.class, args);
-		System.out.println("Hello World!!");
+//		System.out.println("Hello World!!");
 	}
 
 }
---
+++ src/main/java/com/tgt/rysetii/learningresourcesapitanukansal/entity/LearningResources.java
@@ -0,0 +1,90 @@
+package com.tgt.rysetii.learningresourcesapitanukansal.entity;
+import java.time.LocalDate;
+enum LearningResourceStatus {Planning, Published, Live, Retired;}
+
+public class LearningResources {
+    private Integer id;
+    private String name;
+    private Double costPrice;
+    private Double sellingPrice;
+    private LearningResourceStatus productStatus;
+    private LocalDate createdDate;
+    private LocalDate publishedDate;
+    private LocalDate retiredDate;
+
+    public LearningResources(Integer id, String name, Double costPrice, Double sellingPrice, LearningResourceStatus productStatus, LocalDate createdDate, LocalDate publishedDate, LocalDate retiredDate){
+        this.id=id;
+        this.name=name;
+        this.costPrice=costPrice;
+        this.sellingPrice=sellingPrice;
+        this.productStatus=productStatus;
+        this.createdDate=createdDate;
+        this.publishedDate=publishedDate;
+        this.retiredDate=retiredDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getCostPrice() {
+        return costPrice;
+    }
+
+    public void setCostPrice(Double costPrice) {
+        this.costPrice = costPrice;
+    }
+
+    public Double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(Double sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public LearningResourceStatus getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(LearningResourceStatus productStatus) {
+        this.productStatus = productStatus;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDate getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDate publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public LocalDate getRetiredDate() {
+        return retiredDate;
+    }
+
+    public void setRetiredDate(LocalDate retiredDate) {
+        this.retiredDate = retiredDate;
+    }
+
+}
