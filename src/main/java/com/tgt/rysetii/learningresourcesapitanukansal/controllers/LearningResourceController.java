257d72aa7b872a82b5da0c145d5854c808796492
tkansal18 <er.tanukansal@gmail.com>
2022-07-19 23:09:51 +0530

    added get,post and delete endpoint

---
+++ src/main/java/com/tgt/rysetii/learningresourcesapitanukansal/controllers/LearningResourceController.java
@@ -0,0 +1,34 @@
+package com.tgt.rysetii.learningresourcesapitanukansal.controllers;
+
+import com.tgt.rysetii.learningresourcesapitanukansal.entity.LearningResources;
+import com.tgt.rysetii.learningresourcesapitanukansal.service.LearningResourceService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/resources/v1")
+public class LearningResourceController {
+    private final LearningResourceService learningResourceService;
+
+    LearningResourceController(LearningResourceService learningResourceService){
+        this.learningResourceService=learningResourceService;
+    }
+
+    @GetMapping("/")
+    public List<LearningResources> getLearningResources(){
+        return learningResourceService.getLearningResources();
+    }
+
+    @PostMapping(value="/",consumes="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveLearningResources(@RequestBody List<LearningResources> newResource){
+          learningResourceService.saveLearningResources(newResource);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteResource(@PathVariable Integer id){
+        learningResourceService.deleteById(id);
+    }
+}
--- src/main/java/com/tgt/rysetii/learningresourcesapitanukansal/service/LearningResourceService.java
+++ src/main/java/com/tgt/rysetii/learningresourcesapitanukansal/service/LearningResourceService.java
@@ -19,69 +19,10 @@ public class LearningResourceService {
         for(LearningResources lr:resources){
             learningResourceRepository.save(lr);
         }
-//        try{
-//            BufferedWriter bw=new BufferedWriter(new FileWriter("LearningResources.csv",true));
-//            for (LearningResources resource:resources){
-//                bw.newLine();
-//                StringBuffer line=new StringBuffer();
-//                line.append(resource.getId());
-//                line.append(",");
-//                line.append(resource.getName());
-//                line.append(",");
-//                line.append(resource.getCostPrice());
-//                line.append(",");
-//                line.append(resource.getSellingPrice());
-//                line.append(",");
-//                line.append(resource.getProductStatus());
-//                line.append(",");
-//                line.append(resource.getCreatedDate());
-//                line.append(",");
-//                line.append(resource.getPublishedDate());
-//                line.append(",");
-//                line.append(resource.getRetiredDate());
-//            }
-//            bw.flush();
-//            bw.close();
-//
-//        }catch (IOException e){
-//            e.printStackTrace();
-//        }
     }
 
-//    private List<LearningResources> loadLearningResources(String file) {
-//        List<LearningResources> resources= new ArrayList<>();
-//        Path pathToFile= Paths.get(file);
-//        try(BufferedReader br= Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) {
-//            String line=br.readLine();
-//            while (line!=null){
-//                String[] attributes=line.split(",");
-//                LearningResources resource= createResource(attributes);
-//                resources.add(resource);
-//                line=br.readLine();
-//            }
-//        }catch (IOException e){
-//            e.printStackTrace();
-//        }
-//        return resources;
-//
-//    }
-//    private  LearningResources createResource(String[] attributes){
-//        DateTimeFormatter df=DateTimeFormatter.ofPattern("dd-MM-yyyy");
-//        Integer id=Integer.parseInt(attributes[0]);
-//        String name=attributes[1];
-//        Double costPrice=Double.parseDouble(attributes[2]);
-//        Double sellingPrice=Double.parseDouble(attributes[3]);
-//        LearningResourceStatus productStatus=LearningResourceStatus.valueOf(attributes[4]);
-//        LocalDate createdDate=LocalDate.parse(attributes[5],df);
-//        LocalDate publishedDate=LocalDate.parse(attributes[6],df);
-//        LocalDate retiredDate=LocalDate.parse(attributes[7],df);
-//        LearningResources resource=new LearningResources(id,name,costPrice,sellingPrice,productStatus,createdDate,publishedDate,retiredDate);
-//        return resource;
-//    }
     public  List<LearningResources> getLearningResources(){
         return learningResourceRepository.findAll();
-//        List<LearningResources> resources = loadLearningResources("LearningResources.csv");
-//        return resources;
     }
 
     public List<Double> calculateProfitMargin(){
@@ -103,5 +44,8 @@ public class LearningResourceService {
         });
 
         return resources;
+    }
+    public void deleteById(int id) {
+        learningResourceRepository.deleteById(id);
     }
 }
